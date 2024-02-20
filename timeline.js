1f687ad70a8fe072342c2830120e75011636b300
Gold <gold@beyless.com>
2024-02-20 16:58:44 +0900

    clip Event

--- timeline.js
+++ timeline.js
@@ -1,21 +1,44 @@
 const Timeline = class Timeline {
   constructor(canvas, selectedScale) {
     this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
     this.start_x = 0;
-    this.smallBarSize = { x: 2, y: 10 };
-    this.largeBarSize = { x: 2, y: 20 };
-    this.soundDetectBarSize = { x: 2, y: 20 };
-    this.motionDetectBarSize = { x: 2, y: 20 };
-    this.bookMarkBarSize = { x: 5, y: 10 };
+    this.smallBarSize = { x: 2, y: 5 };
+    this.largeBarSize = { x: 2, y: 10 };
+    this.soundDetectBarSize = { x: 2, y: 25 };
+    this.motionDetectBarSize = { x: 2, y: 25 };
+    this.clipMarkStyle = {
+      start : {
+        color: 'rgba(255,255,255,1)',
+        thick: 4,
+        shrink: 4,
+      },
+      area : {
+        color: 'rgba(112,114,119,1)',
+      },
+      end : {
+        color: 'rgba(255,255,255,1)',
+        thick: 4,
+        shrink: 4,
+      },
+    }
+    this.bookMarkBarSize = { w: 7, h: 12, arrowH: 3};
     this.canMove = false;
     this.clickedAxis = null;
+    this.isClipCreate = false;
+    this.createdClip = {
+      clipStart: null,
+      startOffset: null,
+      clipEnd: null,
+      endOffset: null,
+    };
     this.playPoint = new Date(); // 재생시점의 시간 TODO: new Date() 또는 yyyy-MM-dd HH:mm:ss 로 받을 수 있게 작업
     this.playStartPoint = null;
     this.playEndPoint = null;
     this.width = null; 
     this.height = 50; 
     this.mode = 'pc';
-    this.scale = 'oneHour'; // 1분, 10분, 1시간, 3시간, 6시간 등
+    this.scale = 'tenMin'; // 1분, 10분, 1시간, 3시간, 6시간 등
     if(selectedScale) this.scale = selectedScale;
     this.barInterval = null; // 바와 바 사이의 픽셀 간격
     this.unit = null; // 1pixel 당 시간초 : init 또는 scale변경시 초기화
@@ -68,14 +91,16 @@ const Timeline = class Timeline {
         },
       },
     };
+    this.bookmarkPositionList = [];
     this.bookMarkList = [];
     this.soundList = [];
     this.motionList = [];
+    this.clipList = [];
     this.recordList = [];
     this.init();
     this.setTestData(); // 테스트를 위한 임시 랜덤 데이터 생성, 개발 완료 후 제거 예정
   }
-  setTestData() { // 테스트를 위한 임시 랜덤 데이터 생성, 개발 완료 후 제거 예정
+    setTestData() { // 테스트를 위한 임시 랜덤 데이터 생성, 개발 완료 후 제거 예정
     let Point = this.playStartPoint.getTime();
     const loader = (this.playPoint.getTime() - this.playStartPoint.getTime()) / 10;
     for(let g = 0; g < 9; ++ g) {
@@ -84,6 +109,12 @@ const Timeline = class Timeline {
       this.motionList.push(new Date(Point + (Math.random(5, 10) * 200000)));
       this.bookMarkList.push(new Date(Point - (Math.random(3, 10) * 300000)));
     }
+    // this.clipList.push({
+    //   clipStart: new Date(Point - (Math.random(10, 20) * 100000)),
+    //   clipEnd: new Date(Point + (Math.random(10, 20) * 100000)),
+    //   isStart: true, // 시작점이 타임라인 안에 들어가 있는지 확인
+    //   isEnd: true,   // 종료점이 타임라인 안에 들어가 있는지 확인
+    // })
   }
   init() {
     this.width = this.canvas.parentNode.clientWidth;
@@ -94,7 +125,6 @@ const Timeline = class Timeline {
       this.setTimeLineStartPoint();
       this.setTimeLineEndPoint();
     }
-
     this.canvas.addEventListener('mousedown', (e) => {
       if (!this.canMove) {
         this.canMove = true;
@@ -110,18 +140,60 @@ const Timeline = class Timeline {
     });
     this.canvas.addEventListener('mousemove', (e) => {
       if (this.canMove) {
+        this.isClipCreate = false;
         const pp = this.getDragPlayPoint(this.clickedAxis, e.clientX);
         this.clickedAxis = e.clientX;
         this.drawCurrent(pp);
       }
     });
-
     window.addEventListener('resize', (e) => {
       this.width = this.canvas.parentNode.clientWidth;
       this.canvas.width = this.width;
       this.reboot();
       this.drawCurrent(this.playPoint);
     });
+
+    this.canvas.addEventListener('click', e => {
+      const x = e.offsetX;
+      if (this.isClipCreate) {
+        const tmp = this.getClickedClipPlayPoint(x);
+        if (this.createdClip.clipStart === null) {
+          this.createdClip.clipStart = tmp;
+          this.createdClip.startOffset = x;
+          this.createdClip.clipEnd = null;
+          this.createdClip.endOffset = null;
+          this.drawClipStartLine(x);
+          return;
+        }
+        if (this.createdClip.clipStart.getTime() > tmp.getTime()) {
+          this.createdClip.clipStart = tmp;
+          this.createdClip.startOffset = x;
+          this.createdClip.clipEnd = null;
+          this.createdClip.endOffset = null;
+          this.drawClipStartLine(x);
+          return;
+        }
+        if (this.createdClip.clipEnd === null) {
+          this.createdClip.clipEnd = tmp;
+          this.createdClip.endOffset = x;
+          this.drawClipEndLine(x);
+          this.drawClipBox(this.createdClip.startOffset, 0, x, this.canvas.height / 2);
+          return;
+        }
+        this.createdClip.clipStart = tmp;
+        this.createdClip.clipEnd = null;
+        return;
+      }
+      const y = e.offsetY;
+      if (y > this.bookMarkBarSize.h) return;
+      const tmp = this.bookmarkPositionList.filter(ele => {
+        if(x >= ele.AxisXStart && x <= ele.AxisXEnd) return true;
+      })
+      if (tmp.length > 0) {
+        const demandDate = tmp[0].date;
+        console.log('bookmark demandDate', demandDate);
+      }
+    })
   }
   reboot() {
     this.setUnit();
@@ -130,6 +202,16 @@ const Timeline = class Timeline {
       this.setTimeLineStartPoint();
       this.setTimeLineEndPoint();
     }
+    this.canMove = false;
+    this.isClipCreate = false;
+    this.createdClip = {
+      clipStart : null,
+      clipEnd : null,
+    }
+  }
+  getClickedClipPlayPoint(c) {
+    const diff = c * 100 * (this.getScaleSec() / this.getSpacing());
+    return new Date(this.playStartPoint.getTime() + diff);
   }
   getDragPlayPoint(start, end) {
     // start > end : 현재 플레이보다 미래
@@ -141,15 +223,14 @@ const Timeline = class Timeline {
   setBarInterval() {
     const spacing = this.getSpacing();
     let cell = spacing * 10;
-    // if (this.scale === 'oneHour') cell = spacing * 5;
     const largeBarTotalWidth = (spacing - 1) * this.largeBarSize.x;
     const smallBarTotalWidth = spacing * (spacing - 1) * this.smallBarSize.x;
     const remainder = this.width - (largeBarTotalWidth + smallBarTotalWidth);
-    this.barInterval = Number((remainder / cell).toFixed(10)) + this.smallBarSize.x;
+    this.barInterval = Number((remainder / cell).toFixed(8)) + this.smallBarSize.x;
   }
   setUnit() { // 1pixel 당
     const totalSec = this.getScaleSec() * this.getSpacing();
-    this.unit = Number((totalSec / this.width).toFixed(4));
+    this.unit = Number((totalSec / this.width).toFixed(8));
   }
   setTimeLineStartPoint() {
     const minus = (this.width / 2) * this.unit;
@@ -177,78 +258,159 @@ const Timeline = class Timeline {
     return isPlus ? (this.width / 2) - diff : (this.width / 2) + diff;
   }
   drawPlayPointLine() {
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(3, 122, 180, 0.9)';
-    ctx.fillRect(this.width / 2, 0, 2, 100);
+    this.ctx.fillStyle = 'rgba(3, 122, 180, 0.9)';
+    this.ctx.fillRect(this.width / 2, 0, 2, 100);
 
-    ctx.beginPath();
-    ctx.moveTo(this.width / 2 - 4, 0);
-    ctx.lineTo(this.width / 2 + 1, 5);
-    ctx.lineTo(this.width / 2 + 5, 0);
-    ctx.closePath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = 'rgba(3, 122, 180, 0.9)';
-    ctx.stroke();
-    ctx.fillStyle = 'rgba(3, 122, 180, 0.9)';
-    ctx.fill();
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.width / 2 - 4, 0);
+    this.ctx.lineTo(this.width / 2 + 1, 5);
+    this.ctx.lineTo(this.width / 2 + 5, 0);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = 'rgba(3, 122, 180, 0.9)';
+    this.ctx.stroke();
+    this.ctx.fillStyle = 'rgba(3, 122, 180, 0.9)';
+    this.ctx.fill();
 
-    ctx.beginPath();
-    ctx.moveTo(this.width / 2 - 4, this.canvas.height);
-    ctx.lineTo(this.width / 2 + 1, this.canvas.height - 5);
-    ctx.lineTo(this.width / 2 + 5, this.canvas.height);
-    ctx.closePath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = 'rgba(3, 122, 180, 0.9)';
-    ctx.stroke();
-    ctx.fillStyle = 'rgba(3, 122, 180, 0.9)';
-    ctx.fill();
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.width / 2 - 4, this.canvas.height);
+    this.ctx.lineTo(this.width / 2 + 1, this.canvas.height - 5);
+    this.ctx.lineTo(this.width / 2 + 5, this.canvas.height);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = 'rgba(3, 122, 180, 0.9)';
+    this.ctx.stroke();
+    this.ctx.fillStyle = 'rgba(3, 122, 180, 0.9)';
+    this.ctx.fill();
   }
   drawSmallLine(x, y) {
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(156, 157, 160, 0.5)';
-    ctx.fillRect(x, y, this.smallBarSize.x, this.smallBarSize.y);
+    this.ctx.fillStyle = 'rgba(156, 157, 160, 0.5)';
+    this.ctx.fillRect(x, y, this.smallBarSize.x, this.smallBarSize.y);
   }
   drawLargeLine(x, y, text) {
     if (text) {
-      const txt = this.canvas.getContext('2d');
-      txt.font = '10px Arial';
-      txt.fillStyle = 'rgba(150, 150, 150, 1)';
-      txt.fillText(text, x - 12, y - 5);
+      this.ctx.font = '10px Arial';
+      this.ctx.fillStyle = 'rgba(150, 150, 150, 1)';
+      this.ctx.fillText(text, x - 12, y - 5);
     }
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(156, 157, 160, 1)';
-    ctx.fillRect(x, y, this.largeBarSize.x, this.largeBarSize.y);
+    this.ctx.fillStyle = 'rgba(156, 157, 160, 1)';
+    this.ctx.fillRect(x, y, this.largeBarSize.x, this.largeBarSize.y);
   }
   drawSoundDetectLine(x) {
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(116, 65, 198, 1)';
-    ctx.fillRect(x, 0, this.soundDetectBarSize.x, this.soundDetectBarSize.y);
+    this.ctx.lineWidth = 1;
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, 1);
+    this.ctx.lineTo(x, this.soundDetectBarSize.y);
+    this.ctx.strokeStyle = 'rgba(116, 65, 198, 1)';
+    this.ctx.stroke();
+    this.ctx.closePath();
   }
   drawMotionDetectLine(x) {
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(111, 198, 65, 1)';
-    ctx.fillRect(x, 0, this.motionDetectBarSize.x, this.motionDetectBarSize.y);
+    this.ctx.lineWidth = 1;
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, 1);
+    this.ctx.lineTo(x, this.motionDetectBarSize.y);
+    this.ctx.strokeStyle = 'rgba(111, 198, 65, 1)';
+    this.ctx.stroke();
+    this.ctx.closePath();
   }
   drawBookMarkLine(x) {
-    const ctx = this.canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x - this.bookMarkBarSize.x, 0);
-    ctx.lineTo(x - this.bookMarkBarSize.x, this.bookMarkBarSize.y);
-    ctx.lineTo(x, this.bookMarkBarSize.x + this.bookMarkBarSize.y);
-    ctx.lineTo(x + this.bookMarkBarSize.x, this.bookMarkBarSize.y);
-    ctx.lineTo(x + this.bookMarkBarSize.x, 0);
-    ctx.closePath();
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = 'rgba(236, 236, 236, 1)';
-    ctx.stroke();
-    ctx.fillStyle = 'rgba(115, 115, 115, 1)';
-    ctx.fill();
+    const { w, h, arrowH } = this.bookMarkBarSize;
+    const halfW = w / 2;
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, 1);
+    this.ctx.lineTo(x - halfW, 1);
+    this.ctx.lineTo(x - halfW, h);
+    this.ctx.lineTo(x, h + arrowH);
+    this.ctx.lineTo(x + halfW, h);
+    this.ctx.lineTo(x + halfW, 1);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 2.5;
+    this.ctx.strokeStyle = 'rgba(236, 236, 236, 1)';
+    this.ctx.stroke();
+    this.ctx.fillStyle = 'rgba(115, 115, 115, 1)';
+    this.ctx.fill();
   }
   drawBookMark() {
+    this.bookmarkPositionList = [];
     this.bookMarkList
     .filter(ele => this.playStartPoint.getTime() <= ele.getTime() && this.playEndPoint.getTime() >= ele.getTime())
-    .forEach(ele => this.drawBookMarkLine(this.getXAxisOfPoint(ele)));
+    .forEach(ele => {
+      const axisX = this.getXAxisOfPoint(ele);
+      this.drawBookMarkLine(axisX);
+
+      this.bookmarkPositionList.push({
+          date: ele, 
+          AxisXStart: axisX - (this.bookMarkBarSize.w / 2), 
+          AxisXEnd : axisX + (this.bookMarkBarSize.w / 2),
+        });
+    });
+  }
+  drawClipStartLine(x) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(x - this.clipMarkStyle.start.thick, this.clipMarkStyle.start.shrink);
+    this.ctx.lineTo(x, 0);
+    this.ctx.lineTo(x, this.canvas.height / 2 - 1);
+    this.ctx.lineTo(x - this.clipMarkStyle.start.thick, this.canvas.height / 2 - this.clipMarkStyle.start.shrink);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = this.clipMarkStyle.start.color;
+    this.ctx.stroke();
+    this.ctx.fillStyle = this.clipMarkStyle.start.color;
+    this.ctx.fill();
+  }
+  drawClipEndLine(x){
+    this.ctx.beginPath();
+    this.ctx.moveTo(x + this.clipMarkStyle.end.thick, this.clipMarkStyle.end.shrink);
+    this.ctx.lineTo(x, 0);
+    this.ctx.lineTo(x, this.canvas.height / 2 - 1);
+    this.ctx.lineTo(x + this.clipMarkStyle.end.thick, this.canvas.height / 2 - this.clipMarkStyle.end.shrink);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = this.clipMarkStyle.end.color;
+    this.ctx.stroke();
+    this.ctx.fillStyle = this.clipMarkStyle.end.color;
+    this.ctx.fill();
+  }
+  drawClipBox(sx, sy, ex, ey){
+    this.ctx.fillStyle = this.clipMarkStyle.area.color;
+    this.ctx.fillRect(sx, sy, ex - sx, ey);
+  }
+  drawClipArea() {
+    this.clipList.filter(ele => {
+      if (ele.clipEnd.getTime() >= this.playStartPoint.getTime() 
+      && ele.clipStart.getTime() <= this.playEndPoint.getTime()) return true;
+    }).map(ele => {
+      if (ele.clipStart.getTime() < this.playStartPoint.getTime()) {
+        ele.isStart = false;
+        ele.clipStart = this.playStartPoint;
+      }
+      if (ele.clipEnd.getTime() > this.playEndPoint.getTime()) {
+        ele.isEnd = false;
+        ele.clipEnd = this.playEndPoint;
+      }
+      return ele;
+    }).forEach(ele => {
+      const sx = this.getXAxisOfPoint(ele.clipStart);
+      const ex = this.getXAxisOfPoint(ele.clipEnd);
+      this.drawClipBox(sx, 0, ex, this.canvas.height / 2);
+      if (ele.isStart) this.drawClipStartLine(sx);
+      if (ele.isEnd) this.drawClipEndLine(ex);
+    });
+  }
+  drawClickedClipArea() {
+    if (this.createdClip.clipStart === null) return;
+    let sx, ex;
+    if (this.createdClip.clipStart.getTime() > this.playStartPoint.getTime()){
+      sx = this.getXAxisOfPoint(this.createdClip.clipStart);
+      this.drawClipStartLine(sx);
+    }
+    if (this.createdClip.clipEnd === null) return;
+    if (this.createdClip.clipEnd.getTime() < this.playEndPoint.getTime()) {
+      ex = this.getXAxisOfPoint(this.createdClip.clipEnd);
+      this.drawClipEndLine(ex);
+      this.drawClipBox(sx, 0, ex, this.canvas.height / 2);
+    }
   }
   drawMotionMark() {
     this.motionList
@@ -261,16 +423,14 @@ const Timeline = class Timeline {
     .forEach(ele => this.drawSoundDetectLine(this.getXAxisOfPoint(ele)));
   }
   drawRecordBox() {
-    this.drawBlueBox(0, 30, this.width / 2, 20);
+    this.drawBlueBox(0, 25, this.width / 2, 40);
   }
   drawBlueBox(sx, sy, ex, ey) {
-    const ctx = this.canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(20, 151, 212, 0.6)';
-    ctx.fillRect(sx, sy, ex, ey);
+    this.ctx.fillStyle = '#28384c';
+    this.ctx.fillRect(sx, sy, ex, ey);
   }
   drawRefresh() {
-    const ctx = this.canvas.getContext('2d');
-    ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.clearRect(0, 0, this.width, this.height);
   }
   drawTimeScale() {
     let sp = this.playStartPoint.getTime();
@@ -296,27 +456,27 @@ const Timeline = class Timeline {
         largeShare = hour / 6;
         largeRemainder = hour % 6;
         min = 0; // this.width 와 this.unit 에 의해 1분 정도의 오차가 발생할 수 있음
-      } else if (this.scale === 'tenMin') {
+      }  else if (this.scale === 'tenMin') {
         largeShare = min / 10;
         largeRemainder = min % 10;
       } else if (this.scale === 'oneMin') {
         largeShare = min / 1;
         largeRemainder = sec % 60;
-      } 
+      }
       
-      if (!largeRemainder && lastLarge[0] != largeShare && lastLarge[1] !== g) {
+      if (!largeRemainder && lastLarge[0] != largeShare) {
         this.drawLargeLine(
           g,
-          this.canvas.height - 20,
+          this.canvas.height - this.largeBarSize.y,
           `${String(hour).padStart(2, '0')}:${String(min).padStart(2, '0')}`
         );
         
         if (!isFirst) {
           for (let w = lastLarge[1]; w < g; w += this.barInterval)
-            this.drawSmallLine(w, this.canvas.height - 10);
+            this.drawSmallLine(w, this.canvas.height - this.smallBarSize.y);
         } else {
           for (let w = g; w > 0; w -= this.barInterval)
-            this.drawSmallLine(w, this.canvas.height - 10);
+            this.drawSmallLine(w, this.canvas.height - this.smallBarSize.y);
           isFirst = false;
         }
         lastLarge = [largeShare, g];
@@ -325,9 +485,9 @@ const Timeline = class Timeline {
     }
     
     for (let w = lastLarge[1]; w < this.width; w += this.barInterval)
-      this.drawSmallLine(w, this.canvas.height - 10);
+      this.drawSmallLine(w, this.canvas.height - this.smallBarSize.y);
   }
-  drawCurrent() {
+  drawCurrentPlayPoint() {
     this.drawCurrent(this.playPoint);
   }
   drawCurrent(playPoint) {
@@ -337,12 +497,14 @@ const Timeline = class Timeline {
     this.setTimeLineStartPoint();
     this.setTimeLineEndPoint();
 
-    this.drawTimeScale();
-    this.drawBookMark();
+    this.drawClipArea();
     this.drawMotionMark();
     this.drawSoundMark();
-    this.drawPlayPointLine();
+    this.drawBookMark();
     this.drawRecordBox();
+    this.drawTimeScale();
+    this.drawPlayPointLine();
+    this.drawClickedClipArea();
   }
 };
 
@@ -350,20 +512,29 @@ var scaleSelectBox = document.getElementById('scaleSelectBox');
 var canvas = document.getElementById('timeline');
 const timeline = new Timeline(canvas, scaleSelectBox.selectedOptions[0].value);
 const playPoint = new Date();
-logger();
 timeline.drawCurrent(new Date());
-// setInterval(() => {
-//   timeline.drawCurrent(new Date());
-//   logger.innerText = new Date();
-// }, 1000);
+logger();
+setInterval(() => {
+  timeline.drawCurrent(new Date());
+  logger();
+}, 1000);
 function logger() {
   var logger = document.getElementById('logger');
   logger.innerText = timeline.playPoint + ', ' + timeline.mode + ', ' + timeline.scale + ', ' + timeline.unit;
+  logger.innerText += ' \nclip: ' + timeline.isClipCreate + ', ' + timeline.createdClip.clipStart + " / " + timeline.createdClip.clipEnd;
 }
 
 function scaleSelected(select) {
   timeline.scale = select.selectedOptions[0].value;
   timeline.reboot();
   timeline.drawCurrent(timeline.playPoint);
+  logger();
+}
+
+function clipCreate() {
+  timeline.isClipCreate = !timeline.isClipCreate;
+  if (!timeline.isClipCreate) {
+    timeline.reboot();
+  }
   logger();
 }
